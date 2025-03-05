892ffba7e5c46b14abfc84a99a06328ff98a3309
amenallahg122 <bigpapiamen@gmail.com>
2025-03-05 01:45:35 +0100

    Quit

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,9 +1,17 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour {
+public class MainMenu : MonoBehaviour 
+{
     public void PlayMaze()
     {
         SceneManager.LoadScene("maze");
+    }
+
+    public void QuitMaze()
+    {
+        Debug.Log("Quit Game");
+
+        Application.Quit();
     }
 }
